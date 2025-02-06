ee839621634e2f8e6eee28561350de27802a3c32
naopeke <naopeke@protonmail.com>
2025-02-06 14:25:08 +0100

    1-components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
